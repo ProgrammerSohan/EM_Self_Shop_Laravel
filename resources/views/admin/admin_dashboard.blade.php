833e0d5059f67f42b18a275fdae0a6b6c871515b
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-10-20 16:19:55 +0600

    7_admin template setup(a)

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -5,9 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin Dashboard</title>
+    <style>
+
+        
+    </style>
 </head>
 <body>
-    <h1>Admin Dashboard</h1>
+    
+    <div class="main_container">
+        
+        <div class="header">
+            <h1>Admin Dashboard</h1>
+
+        </div> <!-- header end -->
+
+        <div class="sidebar">
+            <h4>Sidebar</h4>
+
+        </div> <!-- sidebar end -->
+
+        <div class="middle">
+            <h4>Majher Content</h4>
+
+        </div> <!-- middle end-->
+
+        <div class="footer">
+            <h4>Footer</h4>
+
+
+        </div>
+
+    </div>
     
 </body>
 <!-- Programmer Sohan -->
